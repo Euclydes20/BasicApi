4ab2b68f48670e1487d8b9278f719ad7380ff732
Euclydes Uchoas dos Santos Neto <euclydes.uchoas@brgaap.com>
2023-10-04 01:44:42 -0300

    Início do desenvolvimento.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,25 +1,54 @@
+using Api;
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddMvc()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddDirectoryBrowser();
+
+Startup.SetAuthenticationService(builder.Services);
+Startup.SetApiDocumentation(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
+app.UseHttpsRedirection();
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
+
+Startup.SetDocumentationCustomization(app);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseCors(x =>
+{
+    x.SetIsOriginAllowed(orgin => true);
+    x.AllowAnyHeader();
+    x.AllowAnyMethod();
+    x.AllowCredentials();
+});
+
+var rewriteOptions = new RewriteOptions();
+rewriteOptions.AddRedirect("^$", "index.html");
+app.UseRewriter(rewriteOptions);
+
+//app.UseSession();
+app.UseAuthentication();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
---
+++ Api/Startup.cs
@@ -0,0 +1,129 @@
+﻿using Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Api
+{
+    public class Startup
+    {
+        internal static void SetAuthenticationService(IServiceCollection services)
+        {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        System.Text.Encoding.ASCII.GetBytes(TokenService.SECRETKEY)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // Retira o tempo extra (geralmente 5 minutos) de validade do token
+                    ClockSkew = TimeSpan.Zero
+                };
+                x.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+
+                        if (string.IsNullOrEmpty(context.Token) &&
+                        (path.StartsWithSegments("/SessionService")))
+                        {
+                            if (!string.IsNullOrEmpty(accessToken))
+                            {
+                                context.Request.Headers["Authorization"] = "Bearer " + accessToken;
+                                context.Token = accessToken;
+                            }
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+
+        internal static void SetApiDocumentation(IServiceCollection services)
+        {
+            var versaoAPI = Assembly.GetEntryAssembly().GetName().Version;
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "LEARN - Euclydes - API Core",
+                    Description = $"API - Versao {versaoAPI}",
+                    Version = "v1 "
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Token de autenticação, após informar usuário e senha copie o token gerado, no botão autorizar, digite: Bearer [seu Token]",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                           // Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+                c.EnableAnnotations();
+
+                //c.OrderActionsBy((apiDesc) => $"{apiDesc.ActionDescriptor.RouteValues["controller"]}_{apiDesc.HttpMethod}");
+                //c.SchemaFilter<SwaggerRemovePropertyFilter>();
+
+                // Set the comments path for the Swagger JSON and UI.
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        internal static void SetDocumentationCustomization(IApplicationBuilder app)
+        {
+
+            var title = Assembly.GetEntryAssembly().GetName().Name;
+            var versaoAPI = Assembly.GetEntryAssembly().GetName().Version;
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "documentation/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/documentation/v1/swagger.json", $"{title} Versão {versaoAPI}");
+                c.RoutePrefix = "documentation";
+
+                c.InjectJavascript("/assets/js/doc.js");
+                c.InjectStylesheet("/assets/css/custom.css");
+                c.DocumentTitle = "BASIC API Core";
+
+                //c.ConfigObject.AdditionalItems.Add("syntaxHighlight", false);
+            });
+        }
+    }
+}
