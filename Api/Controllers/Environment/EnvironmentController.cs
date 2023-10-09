eba28a75e3a1acea08aa10bde5fa5d95a4d551bc
Euclydes Uchoas dos Santos Neto <euclydes.uchoas@brgaap.com>
2023-10-09 01:19:19 -0300

    Endpoint de teste de conexão.

---
+++ Api/Controllers/Environment/EnvironmentController.cs
@@ -0,0 +1,38 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Environment
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Route("v1/[controller]")]
+    [Tags("Environment")]
+    public class EnvironmentController : ControllerBase
+    {
+        public EnvironmentController()
+        {
+            
+        }
+
+        [HttpGet]
+        [Route("Test")]
+        public async Task<IActionResult> Test()
+        {
+            var response = new ResponseInfo<string>();
+            try
+            {
+                response.Data = "Connected";
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return BadRequest(response);
+            }
+        }
+    }
+}
